b5ec1675ea792a7c4463409362c25fe5074c239c
Bangdayeon <dybang00@gmail.com>
2023-06-22 09:11:49 -0400

    230622(not work jQuery)

---
+++ WEB1/js/colors.js
@@ -0,0 +1,44 @@
+var Body = {
+    setColor:function (color){
+        $('body').css('color', color);
+        //document.querySelector('body').style.color = color;
+    },
+
+    setBackgroundColor:function(color){
+        $('body').css('backgroundcolor', color);
+        //document.querySelector('body').style.backgroundColor = color;
+    }
+}
+
+var Links = {
+    setColor:function(color){
+        $('a').css('color', color);
+        //$: 모든 a태그에 대하여 적용하겠다
+
+
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+    }
+}
+
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        Body.setColor('white');
+        Body.setBackgroundColor('black');
+        self.value = 'day';
+
+        Links.setColor('powderblue');
+
+    } else{
+        Body.setColor('black');
+        Body.setBackgroundColor('white');
+        self.value = 'night';
+
+        Links.setColor('blue');
+    }
+}
