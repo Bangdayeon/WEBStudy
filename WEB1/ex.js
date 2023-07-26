e0fdfde9610a9f4d01248729519a3a9c2b17b440
Bangdayeon <dybang00@gmail.com>
2023-07-26 07:27:58 -0400

    js study

---
+++ WEB1/ex.js
@@ -0,0 +1,38 @@
+//문자열
+console.log("[문자열]==========");
+const name = "Bang \'Dayeon"; // 백슬래시로 문자 표현 가능
+const age = 22;
+const msg = `My name is ${name}`; // `는 문자열 내에 변수를 넣을 때 사용. 계산값도 넣을 수 있다
+
+console.log(age);
+console.log(msg);
+
+
+//사칙연산
+console.log("\n[사칙연산]==========");
+const plus = 3+2;
+const minus = 3-2;
+const multiply = 3*2;
+const divide = 3/2;
+
+console.log(plus);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+
+
+//boolean
+console.log("\n[Boolean]==========");
+console.log(true);
+console.log(false);
+console.log(name == "bang");
+console.log(age <23);
+
+
+//null, undefined
+console.log("[null, undefined]==========\n");
+let _undefined;
+let nullUser = null;
+
+console.log(_undefined);
+console.log(nullUser);
